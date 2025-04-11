748df1970fc59d15c6e456f38f2a390c3de946c2
Aditya Mishra <mshradtya@gmail.com>
2025-04-11 15:15:38 +0530

    feat: implment fetch event & list messages

--- index.js
+++ index.js
@@ -6,18 +6,83 @@ class GithubActivity {
     this.rl = readline.createInterface({ input, output });
   }
 
-  #fetchEvents(username) {
-    console.log(`Fetching events for username: ${username}`);
+  URL = "https://api.github.com/users";
+
+  #generateMessages(events) {
+    let messages = [];
+    events.forEach((event) => {
+      let action;
+      switch (event.type) {
+        case "PushEvent":
+          const commitCount = event.payload.commits.length;
+          action = `Pushed ${commitCount} commit(s) to ${event.repo.name}`;
+          break;
+        case "IssuesEvent":
+          action = `${
+            event.payload.action.charAt(0).toUpperCase() +
+            event.payload.action.slice(1)
+          } an issue in ${event.repo.name}`;
+          break;
+        case "WatchEvent":
+          action = `Starred ${event.repo.name}`;
+          break;
+        case "ForkEvent":
+          action = `Forked ${event.repo.name}`;
+          break;
+        case "CreateEvent":
+          action = `Created ${event.payload.ref_type} in ${event.repo.name}`;
+          break;
+        default:
+          action = `${event.type.replace("Event", "")} in ${event.repo.name}`;
+          break;
+      }
+      messages.push(action);
+    });
+
+    return messages;
+  }
+
+  async #fetchEvents(username) {
+    try {
+      console.log(`Fetching events for username: ${username}`);
+      const response = await fetch(`${this.URL}/${username}/events`, {
+        method: "GET",
+      });
+
+      if (response.status === 404) {
+        console.log(`Username: ${username} does not exist`);
+        return [];
+      }
+
+      return await response.json();
+    } catch (err) {
+      console.log("An error occurred while fetching events");
+    }
+  }
+
+  async #handleUserInput(username) {
+    const events = await this.#fetchEvents(username);
+    if (events.length === 0) {
+      console.log(`No public activity found for '${username}'.\n`);
+      this.#getUserInput();
+      return;
+    }
+
+    const messages = this.#generateMessages(events);
+    for (let message of messages) {
+      console.log(message);
+    }
+    this.#getUserInput();
   }
 
   #getUserInput() {
-    this.rl.question("github-activity > ", (answer) => {
+    this.rl.question("github-activity > ", async (answer) => {
+      answer = answer.trim().toLowerCase();
       if (answer === "quit") {
         this.rl.close();
-      } else {
-        this.#fetchEvents(answer);
-        this.#getUserInput();
+        return;
       }
+      this.#handleUserInput(answer);
     });
   }
 
